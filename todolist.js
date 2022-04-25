d0a80f0e6652768a607bd938b5c0c11e746d14dd
osita20 <100966996+osita20@users.noreply.github.com>
2022-04-25 20:20:11 +0100

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -3,11 +3,13 @@ var input = document.getElementById('input');
 var message =document.getElementById('message');
   
 function dex (){
+  if(input.value.length>0){
    var li = document.createElement('li');
    var displaymode = document.createTextNode(input.value);
    li.appendChild(displaymode);
    li.innerHTML +='<button onclick="this.parentNode.remove();"> delete</button >'
    message.appendChild(li);
    input.value= "";
-}
+  };
+};
 btn.addEventListener('click', dex );
